713c0cd79aead95026010b463ac4e97016a45bb4
veom <jordan@ninjatertle.com>
2014-11-16 11:58:01 +1100

    Implemented model updater.

--- RadiantTulip/Model/ModelUpdater.cs
+++ RadiantTulip/Model/ModelUpdater.cs
@@ -10,15 +10,35 @@ namespace RadiantTulip.Model
     {
         private Game _game;
         private DateTime _time;
+        private TimeSpan _increment;
+        private bool _incrementTime;
 
         public ModelUpdater(Game game)
         {
+            if (game.Teams == null || !game.Teams.Any())
+                throw new ArgumentException("You need to have a team");
+            if (!game.Teams.Any(t => t.Players != null && t.Players.Any()))
+                throw new ArgumentException("You must have atleast one player");
+
             _game = game;
+            _time = game.Teams.First().Players.First().Positions.Min(p => p.TimeStamp);
+
+            _increment = game.Teams.First().Players.First().Positions.Where(p => p.TimeStamp != _time).Min(p => p.TimeStamp) - _time;
+
+            _incrementTime = false;
+            Update();
         }
 
         public void Update()
         {
-            throw new NotImplementedException();
+            if (!_incrementTime)
+                _incrementTime = true;
+            else
+                _time += _increment;
+
+            foreach (var t in _game.Teams)
+                foreach (var player in t.Players)
+                    player.CurrentPosition = player.Positions.First(p => p.TimeStamp == _time);
         }
 
         public Game Game
@@ -31,11 +51,20 @@ namespace RadiantTulip.Model
         {
             get
             {
-                throw new NotImplementedException();
+                return _time;
             }
             set
             {
-                throw new NotImplementedException();
+                var minTime = _game.Teams.First().Players.First().Positions.Min(p => p.TimeStamp);
+                var maxTime = _game.Teams.First().Players.First().Positions.Max(p => p.TimeStamp);
+
+                if (value >= minTime && value <= maxTime)
+                {
+                    _time = value;
+                    _incrementTime = false;
+                }                    
+                else
+                    throw new ArgumentException("Time out of range");
             }
         }
     }
--- RadiantTulip/RadiantTulip.Tests/Model/ModelUpdaterTests.cs
+++ RadiantTulip/RadiantTulip.Tests/Model/ModelUpdaterTests.cs
@@ -14,22 +14,13 @@ namespace RadiantTulip.Tests.Model
         [Test]
         public void Game_Getter()
         {
-            var game = new Game();
-            var updater = new ModelUpdater(game);
-
-            Assert.AreEqual(game, updater.Game);
-        }
-
-        [Test]
-        public void Game_Starts_At_Start()
-        {
-            var game = new Game();
+            var game = new Game() { Teams = new List<Team>() };
             var player = new Player
             {
                 Positions = new List<Position>
                 {
-                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 0) },
-                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 10) }
+                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 0) },
+                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 10) }
                 }
             };
             var team = new Team()
@@ -43,19 +34,45 @@ namespace RadiantTulip.Tests.Model
 
             var updater = new ModelUpdater(game);
 
-            Assert.AreEqual(new DateTime(1, 1, 1, 0, 0, 0, 0), updater.Time);
+            Assert.AreEqual(game, updater.Game);
+        }
+
+        [Test]
+        public void Game_Starts_At_Start()
+        {
+            var game = new Game() { Teams = new List<Team>() };
+            var player = new Player
+            {
+                Positions = new List<Position>
+                {
+                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 0) },
+                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 10) }
+                }
+            };
+            var team = new Team()
+            {
+                Players = new List<Player>()
+                {
+                    player
+                }
+            };
+            game.Teams.Add(team);
+
+            var updater = new ModelUpdater(game);
+
+            Assert.AreEqual(new DateTime(2, 1, 1, 0, 0, 0, 0), updater.Time);
         }
 
         [Test]
         public void Player_Starts_At_First_Position()
         {
-            var game = new Game();
+            var game = new Game() { Teams = new List<Team>() };
             var player = new Player
             {
                 Positions = new List<Position>
                 {
-                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 0) },
-                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 10) }
+                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 0) },
+                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 10) }
                 }
             };
             var team = new Team()
@@ -77,13 +94,13 @@ namespace RadiantTulip.Tests.Model
         [Test]
         public void Player_Moves_Next_Position_After_Update()
         {
-            var game = new Game();
+            var game = new Game() { Teams = new List<Team>() };
             var player = new Player
             {
                 Positions = new List<Position>
                 {
-                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 0) },
-                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 10) }
+                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 0) },
+                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 10) }
                 }
             };
             var team = new Team()
@@ -96,6 +113,7 @@ namespace RadiantTulip.Tests.Model
             game.Teams.Add(team);
 
             var updater = new ModelUpdater(game);
+            updater.Update();
 
             var currentPosition = updater.Game.Teams[0].Players[0].CurrentPosition;
             Assert.AreEqual(2, currentPosition.X);
@@ -105,16 +123,16 @@ namespace RadiantTulip.Tests.Model
         [Test]
         public void Player_Moves_Many_Positions_After_Many_Updates()
         {
-            var game = new Game();
+            var game = new Game() { Teams = new List<Team>() };
             var player = new Player
             {
                 Positions = new List<Position>
                 {
-                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 0) },
-                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 10) },
-                    new Position { X = 3, Y = 3, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 20) },
-                    new Position { X = 4, Y = 4, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 30) },
-                    new Position { X = 5, Y = 5, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 40) }
+                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 0) },
+                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 10) },
+                    new Position { X = 3, Y = 3, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 20) },
+                    new Position { X = 4, Y = 4, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 30) },
+                    new Position { X = 5, Y = 5, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 40) }
                 }
             };
             var team = new Team()
@@ -140,13 +158,13 @@ namespace RadiantTulip.Tests.Model
         [Test]
         public void Setting_Time_Stamp_Changes_Time()
         {
-            var game = new Game();
+            var game = new Game() { Teams = new List<Team>() };
             var player = new Player
             {
                 Positions = new List<Position>
                 {
-                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 0) },
-                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 10) }
+                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 0) },
+                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 10) }
                 }
             };
             var team = new Team()
@@ -160,9 +178,10 @@ namespace RadiantTulip.Tests.Model
 
             var updater = new ModelUpdater(game);
             updater.Update();
-            updater.Time = new DateTime(1, 1, 1, 0, 0, 0, 0);
+            updater.Time = new DateTime(2, 1, 1, 0, 0, 0, 0);
+            updater.Update();
 
-            Assert.AreEqual(new DateTime(1, 1, 1, 0, 0, 0, 0), updater.Time);
+            Assert.AreEqual(new DateTime(2, 1, 1, 0, 0, 0, 0), updater.Time);
             var currentPosition = updater.Game.Teams[0].Players[0].CurrentPosition;
             Assert.AreEqual(1, currentPosition.X);
             Assert.AreEqual(1, currentPosition.Y);
@@ -171,13 +190,13 @@ namespace RadiantTulip.Tests.Model
         [Test]
         public void Game_Moves_Smallest_Increment()
         {
-            var game = new Game();
+            var game = new Game() { Teams = new List<Team>() };
             var player = new Player
             {
                 Positions = new List<Position>
                 {
-                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 0) },
-                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 10) }
+                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 0) },
+                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 10) }
                 }
             };
             var team = new Team()
@@ -192,27 +211,27 @@ namespace RadiantTulip.Tests.Model
             var updater = new ModelUpdater(game);
             updater.Update();
 
-            Assert.AreEqual(new DateTime(1, 1, 1, 0, 0, 0, 10), updater.Time);
+            Assert.AreEqual(new DateTime(2, 1, 1, 0, 0, 0, 10), updater.Time);
         }
 
         [Test]
         public void Updates_Multiple_Players()
         {
-            var game = new Game();
+            var game = new Game() { Teams = new List<Team>() };
             var player1 = new Player
             {
                 Positions = new List<Position>
                 {
-                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 0) },
-                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 10) }
+                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 0) },
+                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 10) }
                 }
             };
             var player2 = new Player
             {
                 Positions = new List<Position>
                 {
-                    new Position { X = 3, Y = 3, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 0) },
-                    new Position { X = 4, Y = 4, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 10) }
+                    new Position { X = 3, Y = 3, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 0) },
+                    new Position { X = 4, Y = 4, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 10) }
                 }
             };
             var team = new Team()
@@ -233,28 +252,28 @@ namespace RadiantTulip.Tests.Model
             Assert.AreEqual(2, position1.Y);
 
             var position2 = updater.Game.Teams[0].Players[1].CurrentPosition;
-            Assert.AreEqual(4, position1.X);
-            Assert.AreEqual(4, position1.Y);
+            Assert.AreEqual(4, position2.X);
+            Assert.AreEqual(4, position2.Y);
         }
 
         [Test]
         public void Updates_Multiple_Teams()
         {
-            var game = new Game();
+            var game = new Game() { Teams = new List<Team>() };
             var player1 = new Player
             {
                 Positions = new List<Position>
                 {
-                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 0) },
-                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 10) }
+                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 0) },
+                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 10) }
                 }
             };
             var player2 = new Player
             {
                 Positions = new List<Position>
                 {
-                    new Position { X = 3, Y = 3, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 0) },
-                    new Position { X = 4, Y = 4, TimeStamp = new DateTime(1, 1, 1, 0, 0, 0, 10) }
+                    new Position { X = 3, Y = 3, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 0) },
+                    new Position { X = 4, Y = 4, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 10) }
                 }
             };
             var team1 = new Team()
@@ -283,8 +302,8 @@ namespace RadiantTulip.Tests.Model
             Assert.AreEqual(2, position1.Y);
 
             var position2 = updater.Game.Teams[1].Players[0].CurrentPosition;
-            Assert.AreEqual(4, position1.X);
-            Assert.AreEqual(4, position1.Y);
+            Assert.AreEqual(4, position2.X);
+            Assert.AreEqual(4, position2.Y);
         }
 
         [Test]
@@ -302,20 +321,112 @@ namespace RadiantTulip.Tests.Model
         public void Game_Null_Teams()
         {
             var game = new Game();
-            game.Teams = new List<Team>();
 
             var updater = new ModelUpdater(game);
         }
 
         [Test]
         [ExpectedException(typeof(ArgumentException), ExpectedMessage = "You must have atleast one player")]
-        public void Game_Must_Have_Player()
+        public void Game_Must_Have_Player_Null()
         {
             var game = new Game();
             game.Teams = new List<Team>();
             game.Teams.Add(new Team());
 
             var updater = new ModelUpdater(game);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage = "You must have atleast one player")]
+        public void Game_Must_Have_Player_Empty()
+        {
+            var game = new Game();
+            game.Teams = new List<Team>();
+            game.Teams.Add(new Team() 
+            { 
+                Players = new List<Player>()
+            });
+
+            var updater = new ModelUpdater(game);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage = "Time out of range")]
+        public void Set_Time_Must_Be_Within_Maximum_Position_Range()
+        {
+            var game = new Game() { Teams = new List<Team>() };
+            var player = new Player
+            {
+                Positions = new List<Position>
+                {
+                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 0) },
+                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 10) }
+                }
+            };
+            var team = new Team()
+            {
+                Players = new List<Player>()
+                {
+                    player
+                }
+            };
+            game.Teams.Add(team);
+
+            var updater = new ModelUpdater(game);
+            updater.Time = new DateTime(2, 1, 1, 0, 0, 0, 20);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage = "Time out of range")]
+        public void Set_Time_Must_Be_Within_Minimum_Position_Range()
+        {
+            var game = new Game() { Teams = new List<Team>() };
+            var player = new Player
+            {
+                Positions = new List<Position>
+                {
+                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 10) },
+                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 20) }
+                }
+            };
+            var team = new Team()
+            {
+                Players = new List<Player>()
+                {
+                    player
+                }
+            };
+            game.Teams.Add(team);
+
+            var updater = new ModelUpdater(game);
+            updater.Time = new DateTime(2, 1, 1, 0, 0, 0, 0);
+        }
+
+        [Test]
+        public void Set_Time_Within_Range()
+        {
+            var game = new Game() { Teams = new List<Team>() };
+            var player = new Player
+            {
+                Positions = new List<Position>
+                {
+                    new Position { X = 1, Y = 1, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 10) },
+                    new Position { X = 2, Y = 2, TimeStamp = new DateTime(2, 1, 1, 0, 0, 0, 20) }
+                }
+            };
+            var team = new Team()
+            {
+                Players = new List<Player>()
+                {
+                    player
+                }
+            };
+            game.Teams.Add(team);
+
+            var updater = new ModelUpdater(game);
+            updater.Time = new DateTime(2, 1, 1, 0, 0, 0, 10);
+
+            Assert.AreEqual(new DateTime(2, 1, 1, 0, 0, 0, 10), updater.Time);
         }
     }
 }
