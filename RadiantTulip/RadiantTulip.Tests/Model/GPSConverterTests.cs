dded19db52a234c8252b03c8b83fad71c9fff02d
veom <jordan@ninjatertle.com>
2014-11-19 19:47:46 +1100

    Added a simple test class to explore how the GPSConverter is going to work.

---
+++ RadiantTulip/RadiantTulip.Tests/Model/GPSConverterTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using RadiantTulip.Model;
+using RadiantTulip.Model.Converter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadiantTulip.Tests.Model
+{
+    [TestFixture]
+    public class GPSConverterTests
+    {
+        [Test]
+        public void Test_Converter()
+        {
+            var position = new Position { X = -31.94453984, Y = 115.8308636 };
+            var ground = new Ground();
+            var converter = new GPSConverter();
+
+            var result = converter.Convert(position, ground);
+
+
+        }
+    }
+}
