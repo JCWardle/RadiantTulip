7a2550d74db31da2f8edcc8ae66b11a5da0c3cbd
veom <jordan@ninjatertle.com>
2014-12-07 17:13:52 +1100

    Added the basics for the gameControl custom view.

---
+++ RadiantTulip/RadiantTulip.View/GameControl.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RadiantTulip.View
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:RadiantTulip.View"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:RadiantTulip.View;assembly=RadiantTulip.View"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public class GameControl : ContentControl
+    {
+        private Canvas _canvas;
+        private Table _table;
+
+        static GameControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GameControl), new FrameworkPropertyMetadata(typeof(GameControl)));
+        }
+
+        public GameControl()
+        {
+            _canvas = new Canvas();
+            _canvas.Width = 400;
+            _canvas.Height = 350;
+            AddChild(_canvas);
+            var reader = new FlowDocumentReader();
+            var document = new FlowDocument();
+            reader.Document = document;
+            _table = new Table();
+            document.Blocks.Add(_table);
+        }
+    }
+}
--- RadiantTulip/RadiantTulip.View/GameWindow.xaml.cs
+++ RadiantTulip/RadiantTulip.View/GameWindow.xaml.cs
@@ -16,33 +16,5 @@ namespace RadiantTulip
 {
     public partial class GameWindow : Window
     {
-        private Canvas _canvas;
-        private IModelUpdater _updater;
-        private Game _game;
-        private IGameDrawer _drawer;
-        private Table _table;
-
-        public GameWindow()
-        {
-            _canvas = (Canvas)((StackPanel)this.FindName("Panel")).FindName("MainCanvas");
-            _table = (Table)((FlowDocumentReader)((StackPanel)this.FindName("Panel")).Children[1]).Document.FindName("Table");
-
-            var converter = new GPSConverter();
-            var spatialReader = new ExcelReader();
-            var creator = new GameCreator(converter, spatialReader);
-
-            using (var stream = new FileStream(@"E:\Code\RadiantTulip\TestData\Raw.xlsx", FileMode.Open))
-                _game = creator.CreateGame(stream);
-
-            _updater = new ModelUpdater(_game);
-            _updater.Update();
-
-            _drawer = new GameDrawer(new GroundDrawer(), new PlayerDrawer());
-        }
-
-        private void Draw()
-        {
-            _drawer.DrawGame(_canvas, new Table(), _game);
-        }
     }
 }
