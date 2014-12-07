7a6bb0f7984b7f4eaf58d37e237f064e8d8c28e3
veom <jordan@ninjatertle.com>
2014-12-07 16:47:21 +1100

    Hacked in some values to test how the view is going to work with the model.

---
+++ RadiantTulip/RadiantTulip.View/Game/PlayerDrawer.cs
@@ -0,0 +1,18 @@
+﻿using RadiantTulip.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace RadiantTulip.View.Game
+{
+    public class PlayerDrawer : IPlayerDrawer
+    {
+        public void Draw(Player player, Canvas canvas)
+        {
+            var temp = player;
+        }
+    }
+}
--- RadiantTulip/RadiantTulip.View/GameWindow.xaml.cs
+++ RadiantTulip/RadiantTulip.View/GameWindow.xaml.cs
@@ -1,25 +1,48 @@
-﻿using System;
+﻿using RadiantTulip.Model;
+using RadiantTulip.Model.Converter;
+using RadiantTulip.Model.Input;
+using RadiantTulip.View.Game;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RadiantTulip
 {
     public partial class GameWindow : Window
     {
+        private Canvas _canvas;
+        private IModelUpdater _updater;
+        private Game _game;
+        private IGameDrawer _drawer;
+        private Table _table;
+
         public GameWindow()
         {
-            //InitializeComponent();
+            _canvas = (Canvas)((StackPanel)this.FindName("Panel")).FindName("MainCanvas");
+            _table = (Table)((FlowDocumentReader)((StackPanel)this.FindName("Panel")).Children[1]).Document.FindName("Table");
+
+            var converter = new GPSConverter();
+            var spatialReader = new ExcelReader();
+            var creator = new GameCreator(converter, spatialReader);
+
+            using (var stream = new FileStream(@"E:\Code\RadiantTulip\TestData\Raw.xlsx", FileMode.Open))
+                _game = creator.CreateGame(stream);
+
+            _updater = new ModelUpdater(_game);
+            _updater.Update();
+
+            _drawer = new GameDrawer(new GroundDrawer(), new PlayerDrawer());
+        }
+
+        private void Draw()
+        {
+            _drawer.DrawGame(_canvas, new Table(), _game);
         }
     }
 }
