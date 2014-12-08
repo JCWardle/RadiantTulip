cbd5912c7c685b7f9c4039589c51c849164ff567
veom <jordan@ninjatertle.com>
2014-12-08 18:59:11 +1100

    Added a view model and added a bindable game property to GameControl.

--- RadiantTulip/RadiantTulip.View/GameControl.cs
+++ RadiantTulip/RadiantTulip.View/GameControl.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,39 +16,13 @@ using System.Windows.Shapes;
 
 namespace RadiantTulip.View
 {
-    /// <summary>
-    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
-    ///
-    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:RadiantTulip.View"
-    ///
-    ///
-    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:RadiantTulip.View;assembly=RadiantTulip.View"
-    ///
-    /// You will also need to add a project reference from the project where the XAML file lives
-    /// to this project and Rebuild to avoid compilation errors:
-    ///
-    ///     Right click on the target project in the Solution Explorer and
-    ///     "Add Reference"->"Projects"->[Browse to and select this project]
-    ///
-    ///
-    /// Step 2)
-    /// Go ahead and use your control in the XAML file.
-    ///
-    ///     <MyNamespace:CustomControl1/>
-    ///
-    /// </summary>
     public class GameControl : ContentControl
     {
+        public readonly static DependencyProperty GameProperty = DependencyProperty.Register("Game",  typeof(RadiantTulip.Model.Game), typeof(GameControl), new PropertyMetadata());
+
         private Canvas _canvas;
         private Table _table;
+        private Model.Game _game;
 
         static GameControl()
         {
@@ -65,6 +40,20 @@ namespace RadiantTulip.View
             reader.Document = document;
             _table = new Table();
             document.Blocks.Add(_table);
+        }
+
+        [BindableAttribute(true)]
+        public Model.Game Game 
+        {
+            get 
+            {
+                return _game; 
+            }
+
+            set
+            {
+                value = _game;
+            }
         }
     }
 }
---
+++ RadiantTulip/RadiantTulip.View/ViewModels/GameViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadiantTulip.View.ViewModels
+{
+    public class GameViewModel : BindableBase
+    {
+        private Model.Game _game;
+
+        public Model.Game Game
+        {
+            get
+            {
+                return _game;
+            }
+            
+            set
+            {
+                _game = value;
+            }
+        }
+    }
+}
