832f51a788830b83ba2181f6e63a13ff75e00c92
veom <jordan@ninjatertle.com>
2014-12-08 20:08:30 +1100

    Added an update button and prepared for the game control to draw when the game property is changed.

--- RadiantTulip/RadiantTulip.View/GameControl.cs
+++ RadiantTulip/RadiantTulip.View/GameControl.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RadiantTulip.View.Game;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -18,11 +19,24 @@ namespace RadiantTulip.View
 {
     public class GameControl : ContentControl
     {
-        public readonly static DependencyProperty GameProperty = DependencyProperty.Register("Game",  typeof(RadiantTulip.Model.Game), typeof(GameControl), new PropertyMetadata());
+        public readonly static DependencyProperty GameProperty = DependencyProperty.Register("Game",  
+            typeof(RadiantTulip.Model.Game), 
+            typeof(GameControl), 
+            new PropertyMetadata(new PropertyChangedCallback(Update)));
+
+        private static void Update(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var g = sender as GameControl;
+            if(g != null)
+            {
+                g.OnGameChanged();
+            }
+        }
 
         private Canvas _canvas;
         private Table _table;
         private Model.Game _game;
+        private IGameDrawer _drawer;
 
         static GameControl()
         {
@@ -39,7 +53,21 @@ namespace RadiantTulip.View
             var document = new FlowDocument();
             reader.Document = document;
             _table = new Table();
+            _table.RowGroups.Add(new TableRowGroup());
             document.Blocks.Add(_table);
+
+            var groundDrawer = new GroundDrawer();
+            var playerDrawer = new PlayerDrawer();
+
+            _drawer = new GameDrawer(groundDrawer, playerDrawer);
+        }
+
+        protected virtual void OnGameChanged()
+        {
+            if (_game != null)
+            {
+                _drawer.DrawGame(_canvas, _table, _game);
+            }
         }
 
         [BindableAttribute(true)]
@@ -52,7 +80,7 @@ namespace RadiantTulip.View
 
             set
             {
-                value = _game;
+                _game = value;
             }
         }
     }
--- RadiantTulip/RadiantTulip.View/ViewModels/GameViewModel.cs
+++ RadiantTulip/RadiantTulip.View/ViewModels/GameViewModel.cs
@@ -1,15 +1,50 @@
-﻿using Microsoft.Practices.Prism.Mvvm;
+﻿using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
+using RadiantTulip.Model;
+using RadiantTulip.Model.Converter;
+using RadiantTulip.Model.Input;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace RadiantTulip.View.ViewModels
 {
     public class GameViewModel : BindableBase
     {
         private Model.Game _game;
+        private IModelUpdater _gameUpdater;
+
+        public ICommand UpdateGame
+        {
+            get
+            {
+                return _command ??
+                    (_command = new DelegateCommand(Update));
+            }
+        }
+
+        private DelegateCommand _command;
+
+        private void Update()
+        {
+            _gameUpdater.Update();
+        }
+
+        public GameViewModel()
+        {
+            var converter = new GPSConverter();
+            var reader = new ExcelReader();
+            var creator = new GameCreator(converter, reader);
+
+            using (var stream = new FileStream(@"E:\Code\RadiantTulip\TestData\SmallRaw.xlsx", FileMode.Open))
+                _game = creator.CreateGame(stream);
+
+            _gameUpdater = new ModelUpdater(_game);
+        }
 
         public Model.Game Game
         {
