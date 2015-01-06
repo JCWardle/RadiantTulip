4a0725e8efe1e4e602caf4fd659ab5cb27f7fb09
veom <jordan@ninjatertle.com>
2015-01-06 20:40:15 +1100

    Fixed some player drawer tests.

--- RadiantTulip/RadiantTulip.Tests/View/PlayerDrawerTests.cs
+++ RadiantTulip/RadiantTulip.Tests/View/PlayerDrawerTests.cs
@@ -25,13 +25,16 @@ namespace RadiantTulip.Tests.View
                 };
             var ground = new Ground { Height = 1000, Width = 1000 };
             var canvas = new Canvas { Width = 150, Height = 150 };
+            canvas.Measure(new Size(150, 150));
+            canvas.Arrange(new Rect(0, 0, 150, 150));
             var drawer = new PlayerDrawer();
 
             drawer.Draw(player, ground, canvas);
 
             Assert.AreEqual(1, canvas.Children.Count);
-            var circle = (Ellipse)canvas.InputHitTest(new Point(75, 75));
-            Assert.AreNotEqual(null, circle);
+            var circle = (Ellipse)canvas.Children[0];
+            Assert.AreEqual(75, circle.Margin.Left);
+            Assert.AreEqual(75, circle.Margin.Top);
             Assert.AreEqual(5, circle.Width);
             Assert.AreEqual(5, circle.Height);
         }
@@ -48,13 +51,17 @@ namespace RadiantTulip.Tests.View
                     }
                 };
             var ground = new Ground { Height = 500, Width = 1000 };
-            var canvas = new Canvas { Width = 150, Height = 150 };
+            var canvas = new Canvas { Width = 300, Height = 150 };
+            canvas.Measure(new Size(300, 150));
+            canvas.Arrange(new Rect(0, 0, 300, 150));
             var drawer = new PlayerDrawer();
             
             drawer.Draw(player, ground, canvas);
             Assert.AreEqual(1, canvas.Children.Count);
-            var circle = (Ellipse)canvas.InputHitTest(new Point(37.5, 90));
+            var circle = (Ellipse) canvas.Children[0];
             Assert.AreNotEqual(null, circle);
+            Assert.AreEqual(75, circle.Margin.Left);
+            Assert.AreEqual(90, circle.Margin.Top);
             Assert.AreEqual(5, circle.Width);
             Assert.AreEqual(5, circle.Height);
         }
