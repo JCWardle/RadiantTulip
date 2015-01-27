7447660b4da0b26c65b837e54434475b22c58e8a
veom <jordan@ninjatertle.com>
2015-01-27 20:57:22 +1100

    Made the context window change depending on which players are selected.

---
+++ RadiantTulip/RadiantTulip.View/Converters/MultipleSelectConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RadiantTulip.View.Converters
+{
+    public class MultipleSelectConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var count = (int)value;
+
+            return (count > 1 ? Visibility.Visible : Visibility.Collapsed);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var visibility = (Visibility)value == Visibility.Visible;
+
+            return (visibility ? 2 : 1);
+        }
+    }
+}
