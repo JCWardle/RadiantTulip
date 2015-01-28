8c57ef2470010825829c176868893e4e8ad9bca2
veom <jordan@ninjatertle.com>
2015-01-28 19:51:15 +1100

    Made it so that when a player is selected from the ground they appear selected in the list on the left.

---
+++ RadiantTulip/RadiantTulip.View/Converters/IsSelected.cs
@@ -0,0 +1,27 @@
+﻿using RadiantTulip.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RadiantTulip.View.Converters
+{
+    public class IsSelected : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var player = (Player)values[0];
+            var collection = (ObservableCollection<Player>)values[1];
+
+            return collection.Contains(player);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- RadiantTulip/RadiantTulip.View/GameWindow.xaml.cs
+++ RadiantTulip/RadiantTulip.View/GameWindow.xaml.cs
@@ -4,13 +4,17 @@ using RadiantTulip.Model;
 using RadiantTulip.View.Game;
 using RadiantTulip.View.ViewModels;
 using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Linq;
+using System.Diagnostics;
 
 namespace RadiantTulip.View
 {
@@ -35,6 +39,26 @@ namespace RadiantTulip.View
         protected void ReRender()
         {
             _drawer.DrawGame(Game, _table, _view.Game);
+        }
+
+        private void Game_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            var element = Game.InputHitTest(Mouse.GetPosition(Game));
+            var game = _view.Game;
+
+            if (element is Shape)
+            {
+                var shape = (FrameworkElement)element;
+                var x = (game.Ground.Width * (shape.Margin.Left + shape.Width / 2)) / Game.ActualWidth;
+                var y = (game.Ground.Height * (shape.Margin.Top + shape.Height / 2)) / Game.ActualHeight;
+
+                foreach (var t in game.Teams)
+                {
+                    var player = t.Players.FirstOrDefault(p => Math.Round(p.CurrentPosition.X, 0) == Math.Round(x, 0) && Math.Round(p.CurrentPosition.Y, 0) == Math.Round(y, 0));
+                    if(player != null)
+                        _view.SelectedPlayers.Add(player);
+                }
+            }
         }
     }
 }
