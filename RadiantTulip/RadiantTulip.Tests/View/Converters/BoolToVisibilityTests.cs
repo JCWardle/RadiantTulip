827610658b0dd03fbba2e62d93ec0b5a4eea7c41
veom <jordan@ninjatertle.com>
2015-04-12 21:05:44 +1000

    Added test coverage for BoolToVisibility converter.

---
+++ RadiantTulip/RadiantTulip.Tests/View/Converters/BoolToVisibilityTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using RadiantTulip.View.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RadiantTulip.Tests.View.Converters
+{
+    [TestFixture]
+    public class BoolToVisibilityTests
+    {
+        [Test]
+        public void Converts_True_To_Visible()
+        {
+            var converter = new BoolToVisibility();
+
+            var result = (Visibility)converter.Convert(true, typeof(Visibility), null, null);
+
+            Assert.AreEqual(Visibility.Visible, result);
+        }
+
+        [Test]
+        public void Converts_False_To_Collapsed()
+        {
+            var converter = new BoolToVisibility();
+
+            var result = converter.Convert(false, typeof(Visibility), null, null);
+
+            Assert.AreEqual(Visibility.Collapsed, result);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Visibility_Doesnt_Convert_Object()
+        {
+            var converter = new BoolToVisibility();
+
+            var result = converter.Convert(new object(), typeof(Visibility), null, null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void Visibility_Convert_Back()
+        {
+            var converter = new BoolToVisibility();
+
+            var result = converter.ConvertBack(false, typeof(Visibility), null, null);
+        }
+    }
+}
--- RadiantTulip/RadiantTulip.View/Converters/BoolToVisibility.cs
+++ RadiantTulip/RadiantTulip.View/Converters/BoolToVisibility.cs
@@ -12,8 +12,12 @@ namespace RadiantTulip.View.Converters
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            var b = (bool)value;
-            return b ? Visibility.Visible : Visibility.Collapsed;
+            var b = value as bool?;
+
+            if (b == null)
+                throw new ArgumentException();
+
+            return b.Value ? Visibility.Visible : Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
