c18a2055f77dca61d2eac88fdd409b73c0cee9cc
veom <jordan@ninjatertle.com>
2015-05-19 20:26:38 +1000

    Added the player's colour next to their name in the menu on the left so they are easily identified.

--- RadiantTulip/RadiantTulip.Model/Player.cs
+++ RadiantTulip/RadiantTulip.Model/Player.cs
@@ -1,17 +1,41 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Windows.Media;
 
 namespace RadiantTulip.Model
 {
-    public class Player
+    public class Player : INotifyPropertyChanged
     {
         public Team Team { get; set; }
         public bool Visible { get; set; }
         public List<Position> Positions { get; set; }
         public Position CurrentPosition { get; set; }
         public string Name { get; set; }
-        public Color Colour { get; set; }
+        private Color _colour;
+        public Color Colour
+        {
+            get
+            {
+                return _colour;
+            }
+            set
+            {
+                _colour = value;
+                OnPropertyChanged("Colour");
+            }
+        }
         public Size Size { get; set; }
         public PlayerShape Shape { get; set; }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
---
+++ RadiantTulip/RadiantTulip.Tests/Model/PlayerTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using RadiantTulip.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace RadiantTulip.Tests.Model
+{
+    [TestFixture]
+    public class PlayerTests
+    {
+        [Test]
+        public void Colour_Set_Notification()
+        {
+            var player = new Player() { Colour = Color.FromArgb(255, 255, 255, 255) };
+            var eventTriggered = false;
+
+            player.PropertyChanged +=
+                delegate(object sender, PropertyChangedEventArgs e)
+                {
+                    eventTriggered = true;
+                    Assert.AreEqual("Colour", e.PropertyName);
+                };
+
+            player.Colour = Color.FromArgb(0, 0, 0, 0);
+
+            Assert.IsTrue(eventTriggered);
+        }
+    }
+}
